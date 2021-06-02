33ee85d2ee923b47716b36d5e56f750833986887
Sergio Chin-Sue <s.chinsue@gmail.com>
2021-06-02 23:35:53 +0100

    Add documentation for new matrix methods

--- lib/matrix.ex
+++ lib/matrix.ex
@@ -3,6 +3,10 @@ defmodule Matrix do
   Documentation for `Matrix`.
   """
 
+  defp get_matrix(matrix) do
+    matrix |> Enum.reverse
+  end
+
   @doc """
   Creates a new matrix.
 
@@ -22,10 +26,28 @@ defmodule Matrix do
     for _ <- 1..x, do: (for _ <- 1..y, do: 0)
   end
 
-  defp get_matrix(matrix) do
-    matrix |> Enum.reverse
-  end
+  @doc """
+  Replaces a row in the matrix
 
+  ## Examples
+
+      iex> matrix = Matrix.new(5, 5)
+      [
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+      ]
+      iex> matrix |> Matrix.replace_at(0,0,"E")
+      [
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        ["E",0,0,0,0],
+      ]
+  """
   def replace_at(matrix, x, y, val) do
     new_row = matrix
       |> Enum.at(y)
@@ -33,6 +55,23 @@ defmodule Matrix do
 
     matrix |> List.replace_at(y, new_row) |> Enum.reverse
   end
+
+  @doc """
+  Gets element in the matrix
+
+  ## Examples
+
+      iex> matrix = Matrix.new(5, 5)
+      [
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+      ]
+      iex> matrix |> Matrix.replace_at(0,0,"E") |> Matrix.at(0,0)
+      "E"
+  """
 
   def at(matrix, x, y) do
     matrix
